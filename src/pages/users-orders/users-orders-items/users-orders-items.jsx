ce34c9ad27d2edfa2dbc2e88c7aa8c37c06dc09d
Shair <sashabazarbaev@mail.ru>
2019-11-23 20:22:24 +0300

    upd

--- src/pages/users-orders/users-orders-items/users-orders-items.jsx
+++ src/pages/users-orders/users-orders-items/users-orders-items.jsx
@@ -5,7 +5,7 @@ const UsersOrdersItems = props => {
 	return (
 		<div className="order">
 			<div className="order__title">{title}</div>
-			<div>Количество: {count}</div>
+			<div className="blue-text">Количество: {count}</div>
 		</div>
 	)
 }
