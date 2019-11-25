380c299e196e5c4b8f7e46a7c886e9fb91651605
Shair <sashabazarbaev@mail.ru>
2019-11-25 03:13:08 +0300

    fixed bug

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import store from './data/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import 'materialize-css/dist/css/materialize.css'
+import 'materialize-css/dist/js/materialize'
 
 ReactDOM.render(
 	<Provider store={store}>
